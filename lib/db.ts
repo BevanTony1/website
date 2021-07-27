a126979970798270b1b1d4bc5044681d400ddd51
Bevan Tony Medrano <medranobevantonye@gmail.com>
2021-07-27 22:00:28 +0800

    setting up firebase auth, firestore, chakra-ui, _app and _document

---
+++ lib/db.ts
@@ -0,0 +1,8 @@
+import firebase from './firebase'
+
+const firestore = firebase.firestore();
+
+export function createUser(uid: string, data: object) {
+    return firestore.collection('users').doc(uid).set({ uid, ...data }, { merge: true })
+
+}
