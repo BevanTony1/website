fccbe0deb2297b3dbb9dd4df5c95891954af1b61
Bevan Tony Medrano <medranobevantonye@gmail.com>
2021-08-03 12:52:44 +0800

    conditioning for spotify plying now

--- components/SpotifyNowPlaying.tsx
+++ components/SpotifyNowPlaying.tsx
@@ -1,39 +1,34 @@
 import styles from '../styles/Spotify.module.scss'
-import { Box, Center, Text, Stack, Flex } from '@chakra-ui/react'
-import Image from 'next/image'
+import { Box, Center, Text, Stack, Flex, Image } from '@chakra-ui/react'
+// import Image from 'next/image'
 import SpotifySkeleton from './SpotifySkeleton'
 import useSWR from 'swr'
 
 const SpotifyNowPlaying = () => {
 
-
   const { data, error } = useSWR('api/spotify')
 
-  if (!data) {
-    return <SpotifySkeleton />
+
+  if (!data || data.isPlaying == false) {
+    return (
+      <Box>
+        <Text>Not Playing...</Text>
+      </Box>
+    )
   }
 
-
-  console.log(data)
-
   return (
-    <Center>
-      <Box textAlign='left' border='1px' width={{ lg: '80%', md: '50%', base: '100%' }} borderRadius='25px'>
-        <Center>
 
-          <Flex flexDirection='column' paddingTop='5'>
-            <div className={styles.imageContainer}>
-              <Image src={data.albumImageUrl} height='150' width='150' />
-            </div>
-            <Stack fontFamily='monospace' paddingBottom='4'>
-              <Text paddingTop='2'>Album: {data.album}</Text>
-              <Text>Title: {data.title}</Text>
-              <Text>Artist: {data.artist}</Text>
-            </Stack>
-          </Flex>
-        </Center>
-      </Box >
-    </Center >
+    <Flex flexDirection='row' paddingLeft='5' paddingTop='5'>
+      <div className={styles.imageContainer}>
+        <Image src={data.albumImageUrl} height='150' width='150' />
+      </div>
+      <Stack paddingLeft='5' display='flex' justifyContent='center' fontFamily='monospace' paddingBottom='4'>
+        <Text paddingTop='2'>Album: {data.album}</Text>
+        <Text>Title: {data.title}</Text>
+        <Text>Artist: {data.artist}</Text>
+      </Stack>
+    </Flex>
   )
 }
 export default SpotifyNowPlaying
